7a5e40f26c2f2678f242ba418d34ef8b7ea25582
iwshan <iwshan@sina.com>
2013-02-18 18:45:23 +0800

    add SearchJobView

---
+++ SearchJobView.m
@@ -0,0 +1,31 @@
+//
+//  SearchJobView.m
+//  ZLEmploy
+//
+//  Created by wusan on 13-2-18.
+//  Copyright (c) 2013年 alex. All rights reserved.
+//
+
+#import "SearchJobView.h"
+
+@implementation SearchJobView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
